adffc6a7639c2327413810b8631d11a5d59c2ea9
mikevercel <mikevercel@users.noreply.github.com>
2026-04-12 17:54:47 -0700

    consolidate config

--- home/dot_config/nvim/lua/plugins/init.lua
+++ home/dot_config/nvim/lua/plugins/init.lua
@@ -1,7 +1,10 @@
 return {
+	-- disable default plugins
 	{ "folke/flash.nvim", enabled = false },
 	{ "catppuccin/nvim", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
+
+	-- snacks
 	{
 		"folke/snacks.nvim",
 		opts = {
@@ -30,6 +33,40 @@ return {
 				enabled = true,
 			},
 			explorer = { enabled = false },
+		},
+	},
+
+	-- dracula
+	{
+		"Mofiqul/dracula.nvim",
+		lazy = true,
+		opts = {
+			-- customize dracula theme colors
+			colors = {
+				bg = "#000000",
+				selection = "#202040",
+			},
+		},
+	},
+
+	-- yazi
+	{
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>E",
+				function()
+					require("yazi").yazi()
+				end,
+				desc = "Open Yazi",
+			},
+		},
+		opts = {
+			-- Enable yazi to replace netrw for directory opening
+			open_for_directories = true,
+			-- Fullscreen mode
+			yazi_floating_window_scaling_factor = 1.0,
 		},
 	},
 }
